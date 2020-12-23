dd5c1478e981597fc5c52d24e727cdb3d759fb1b
Shane meek <37853304+KrazyCrave@users.noreply.github.com>
2020-12-22 21:14:16 -0500

    TFF v3.3

--- admin/warn.js
+++ admin/warn.js
@@ -3,15 +3,17 @@ module.exports = {
     description: "This mutes a member",
     execute(message, args) {
         const target = message.mentions.users.first();
+        
 
         if (target){
 
             let memberTarget = message.guild.members.cache.get(target.id);
+           
 
             if(message.member.roles.cache.has('776281276138782760')){
 
                 if (!args[1]) {
-                    message.channel.send(`<@${memberTarget.user.id}> has been warned`);
+                    message.channel.send(`<@${memberTarget.user.id}> has recieved a warning`);
                     memberTarget.send('You have failed to follow one of the rules so you have recieved a warning')
                     return
                 }
